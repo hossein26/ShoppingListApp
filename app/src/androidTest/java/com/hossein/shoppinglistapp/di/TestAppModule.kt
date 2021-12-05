eb5270a61699c058976dcfe74c53f9a35aafede3
hossein26 <hosseinbeigi26@gmail.com>
2021-12-05 08:53:09 +0330

    Testing with Dagger-Hilt

---
+++ app/src/androidTest/java/com/hossein/shoppinglistapp/di/TestAppModule.kt
@@ -0,0 +1,40 @@
+package com.hossein.shoppinglistapp.di
+
+import android.content.Context
+import androidx.room.Room
+import com.hossein.shoppinglistapp.data.local.ShoppingItemDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TestAppModule {
+
+    @Provides
+    @Named("test_db")
+    fun provideInMemoryDb(@ApplicationContext context: Context) =
+        Room.inMemoryDatabaseBuilder(context, ShoppingItemDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
